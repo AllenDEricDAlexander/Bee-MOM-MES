4294a72c7f05577d39e245c5fae8b04909582b8d
luofu <mqa010225@163.com>
2023-11-13 17:00:04 +0800

    modify swagger info configuration

--- code/BEE_MOM/master_data/src/main/java/top/atluofu/master_data/config/SwaggerConfig.java
+++ code/BEE_MOM/master_data/src/main/java/top/atluofu/master_data/config/SwaggerConfig.java
@@ -1,9 +1,14 @@
 package top.atluofu.master_data.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 /**
  * @ClassName: SwaggerConfig
@@ -18,9 +23,22 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI springOpenAPI() {
-        return new OpenAPI().info(new Info()
-                .title("SpringDoc API Test")
-                .description("SpringDoc Simple Application Test")
-                .version("0.0.1"));
+        Server devServer = new Server();
+        devServer.setUrl("www.atluofu.com/masterData");
+        devServer.setDescription("Server URL in Development environment");
+        Server prodServer = new Server();
+        prodServer.setUrl("www.atluofu.cn/masterData");
+        Contact contact = new Contact();
+        contact.setEmail("mqa010225@gmail.com");
+        contact.setName("Allen-D-Alexander");
+        contact.setUrl("https://www.atluofu.top");
+        License gpLv3License = new License().name("GNU GPLv3 License").url("https://www.gnu.org/licenses/gpl-3.0-standalone.html");
+        Info info = new Info()
+                .title("masterData API")
+                .version("0.0.1-SNAPSHOT")
+                .contact(contact)
+                .description("This API exposes endpoints to manage tutorials.").termsOfService("https://www.atluofu.top")
+                .license(gpLv3License);
+        return new OpenAPI().info(info).servers(List.of(devServer, prodServer));
     }
 }
