e4769f5cb6523e3542ff5a8761a7f61a87f103b7
luofu <mqa010225@163.com>
2023-11-17 09:02:16 +0800

    Revert "nacos remote center configuration"
    This reverts commit f4646aa070e9e1922a978f7c16b764005cc80bdd.

--- code/BEE_MOM/wms_model/src/main/java/top/atluofu/wms_model/WMSModelApplication.java
+++ code/BEE_MOM/wms_model/src/main/java/top/atluofu/wms_model/WMSModelApplication.java
@@ -1,6 +1,5 @@
 package top.atluofu.wms_model;
 
-
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
@@ -16,7 +15,7 @@ import java.net.InetAddress;
 
 /**
  * @ClassName: WMSModelApplication
- * @description: WMSModelApplication
+ * @description: TODO
  * @author: 有罗敷的马同学
  * @datetime: 2023Year-11Month-07Day-8:58
  * @Version: 1.0
